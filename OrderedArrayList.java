287e70ee9eab86648c4c3454c1203f2e3e15258b
mschneider30 <mschneider30@stuy.edu>
2021-12-13 13:18:36 -0500

    Added OAL

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 
 public class OrderedArrayList {
-  
+
   //instance variables
   private ArrayList<Integer> _data;
-  
+
   //default constructor -- initializes new ArrayList that holds only int values
   public OrderedArrayList() {
     _data = new ArrayList<Integer>();
@@ -35,86 +35,74 @@ public class OrderedArrayList {
     return this._data.get(index);
   }
 
-  //method that checks whether the ArrayList is properly sorted
-  public boolean testOrdered () {
-    //iterates through ArrayList
-    for (int r = 0; r < this._data.size()-1; r++) {
-      //checks if value at current index is larger than value at next index -- returns false if true / continues iteration if false
-      if (this._data.get(r) > this._data.get(r+1)) {
-          return false;
-        }
-    }
-    //returns true if iteration is complete
-    return true;
-  }
 
-  //method that adds value 'a' into ArrayList
-  public boolean add(int a) {
-    //iterates through ArrayList
-    for (int i = 0; i <= this._data.size();i++) {
-      //adds value at specified index
-      this._data.add(i, a);
-      //checks if ArrayList is ordered
-      if (this.testOrdered()) {
-        //stop method
-        return true;
-      }
-      else {
-        //if not ordered, remove value at index and continue iteration
-        this._data.remove(i);
-      }
-    }
-    
-    //accounts for case if ArrayList is empty
-    if(this._data.size() == 0) {
-      //add value at index 0, stop method
-      this._data.add(0, a);
-      return true;
-    }
-    //ensures that method is guaranteed to have boolean returnType
-    return false;
-  }
 
   //removes value at specified index
   public void remove(int index) {
     this._data.remove(index);
   }
 
-  //replaces value at specified index with specified value
-  public void set(int index, int input) {
-    this.remove(index);
-    this.add(input);
+
+  // inserts newVal at the appropriate index
+  // maintains ascending order of elements
+  // uses a linear search to find appropriate index
+  public void addLinear(Integer newVal)
+  {
+    int c = 0;
+    for(int i = 0;c==0 && i < this._data.size(); i++) {
+      if (newVal <= this._data.get(i)) {
+        this._data.add(i,newVal);
+        c++;
+      }
+    }
+    if (c == 0) {
+      this._data.add(newVal);
+    }
   }
 
-
-  public static void main (String[] args) {
-    OrderedArrayList tester = new OrderedArrayList();
-    System.out.println(tester);
-
-    tester.add((int)(Math.random()*100));
-    tester.add((int)(Math.random()*100));
-    tester.add((int)(Math.random()*100));
-    tester.add((int)(Math.random()*100));
-    tester.add((int)(Math.random()*100));
-
-    System.out.println(tester);
-
-    System.out.println("Size of Tester: " + tester.size());
-    System.out.println("Third Item in List: " + tester.get(2));
-
-    int a = 1;
-    int b = 2;
-    int c = 3;
-    int d = 4;
-    int e = 5;
-
-    tester.set(0,c);
-    tester.set(1,b);
-    tester.set(2,e);
-    tester.set(3,a);
-    tester.set(4,d);
-
-    System.out.println("Test of set method. It should replace all 5 numbers in tester with the numbers 1-5 sorted correctly: " + tester);
-
+  // inserts newVal at the appropriate index
+  // maintains ascending order of elements
+  // uses a binary search to find appropriate index
+  public boolean addBinary(Integer newVal)
+  {
+    if (this._data.size() == 0) {
+      this._data.add(newVal);
+      return true;
+    }
+    int c = this._data.size() / 2;
+  while ( c < this._data.size()) {
+    int d= c + 1;
+    if (newVal <= this._data.get(d) && newVal >=this._data.get(c)) {
+      this._data.add(d, newVal);
+      return true;
+    } else if (newVal > this._data.get(d)) {
+      c+=1;
+      return true;
+    } else {
+      c-=1;
+      return true;
+    }
   }
+  return false;
+  }
+
+  // main method solely for testing purposes
+  public static void main( String[] args )
+  {
+
+    OrderedArrayList Franz = new OrderedArrayList();
+    // testing linear search
+    for( int i = 0; i < 15; i++ )
+      Franz.addLinear( (int)( 50 * Math.random() ) );
+    System.out.println( Franz );
+    // testing binary search
+    Franz = new OrderedArrayList();
+    for( int i = 0; i < 15; i++ )
+      Franz.addBinary( (int)( 50 * Math.random() ) );
+    System.out.println( Franz );
+
+  }//end main()
+
+
+
 }
