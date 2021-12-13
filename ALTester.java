165a367fee992d578880512f4a5a8ba328562927
David Deng <78229116+daviddeng78@users.noreply.github.com>
2021-12-12 22:50:16 -0500

    added indentation and comments

--- ALTester.java
+++ ALTester.java
@@ -1,27 +1,31 @@
 import java.util.ArrayList;
+
 public class ALTester {
 
-public static boolean testOrdered (ArrayList<Integer> foo) {
-for (int r = 0; r < foo.size()-1;r++) {
-if (foo.get(r) > foo.get(r+1)) {
-return false;
-}}
-return true;
-}
-public static void main (String[] args) {
+  //checks whether ArrayList is sorted
+  public static boolean testOrdered (ArrayList<Integer> foo) {
+    for (int r = 0; r < foo.size()-1; r++) {
+      if (foo.get(r) > foo.get(r+1)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  public static void main (String[] args) {
 
-ArrayList<Integer> fooo = new ArrayList<Integer>();
-for (int i = 0;i < 23; i ++) {
-fooo.add(i,(int)(Math.random()*100));
-}
+    ArrayList<Integer> fooo = new ArrayList<Integer>();
+    for (int i = 0;i < 23; i ++) {
+      fooo.add(i,(int)(Math.random()*100));
+    }
 
-ArrayList<Integer> foooo = new ArrayList<Integer>();
-for (int i = 0;i < 23; i ++) {
-foooo.add(i,i);
-}
+    ArrayList<Integer> foooo = new ArrayList<Integer>();
+    for (int i = 0;i < 23; i ++) {
+      foooo.add(i,i);
+    }
 
-System.out.println("Should be false: " + testOrdered(fooo));
-System.out.println("Should be true: " + testOrdered(foooo));
+    System.out.println("Should be false: " + testOrdered(fooo));
+    System.out.println("Should be true: " + testOrdered(foooo));
 
-}
+  }
 }
